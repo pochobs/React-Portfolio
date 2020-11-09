02172a3ddf69b5432b0743cf7d95f1868ef22569
Pochobs <pochobs@hotmail.com>
2020-11-08 23:14:43 -0600

    Updates

---
+++ precache-manifest.6d71ecce6c9330962f177e19f4f48c16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148e4275563e60772d4674591327c2a7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3f414b5ff8903733611c",
+    "url": "/react-portfolio/static/css/main.656d3a8b.chunk.css"
+  },
+  {
+    "revision": "24ddec1d92d946323b7d",
+    "url": "/react-portfolio/static/js/2.6e86267e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.6e86267e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f414b5ff8903733611c",
+    "url": "/react-portfolio/static/js/main.916cbe63.chunk.js"
+  },
+  {
+    "revision": "42e6e07fd3dcb766db6b",
+    "url": "/react-portfolio/static/js/runtime-main.14f01a53.js"
+  },
+  {
+    "revision": "1c102530e5761e66342e6bac2ae42e46",
+    "url": "/react-portfolio/static/media/Alfonso-Resume.1c102530.docx"
+  },
+  {
+    "revision": "66d989b43780c24c4d24b576efd34fa8",
+    "url": "/react-portfolio/static/media/portfolio-image.66d989b4.jpg"
+  }
+]);
